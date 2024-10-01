e5e340b3874277ee1dde13c9cfd584856b2425ba
Gabriel Erick <gabrielmmve13@gmail.com>
2024-10-01 15:20:58 -0300

    refazendo view de produtos com bootstrap

--- resources/views/produto.blade.php
+++ resources/views/produto.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.site')
-<link rel="stylesheet" href="{{ asset('css/siteCss/produtos.css') }}">
+<link rel="stylesheet" href="{{ asset('css/posicionaBotaoSubmit.css') }}">
 
 <!-- adicionando fonte 2 (Titan One) -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,90 +13,104 @@
             background-color: #F8F7F7;
         }
 
+        .posicionaBotaoSubmit{
+            justify-content: space-between;
+            padding-right: 1rem;
+            margin-top: 0
+        }
     </style>
     <main>
 
-        <div class="imgTelaProduto">
-            <img src="{{ asset('img/duplo-cheddar.png') }}">
+        <div class="text-center" style="background-color: #fbe7ca;">
+            <img src="{{ asset('img/duplo-cheddar.png') }}" style="width: 50vw; height: 70vh">
         </div>
 
+        <div class="">
 
-        <div class="especificacoesProduto">
+            <div class="px-3">
+                <div class="d-flex justify-content-between w-100 my-2">
+                    <h1 style="font-family: 'Titan One', sans-serif; color:#8C6342" class="fw-normal">Mrs. King Chedar Extra
+                    </h1>
 
-            <div class="detalhesProduto centraliza">
-                <div id="valorNomeTelaProduto">
-                    <h1>Mrs. King Chedar Extra</h1>
-
-                    <h2>$38.99</h2>
+                    <h2 style="font-family: 'Poppins', sans-serif; font-weight:600">$38.99</h2>
                 </div>
 
-                <div class="descricaoTelaProduto">
+                <div style="font-family: 'Cabin', sans-serif; font-weight: 500; color: #979797">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aperiam debitis dolores porro
                         magnam quos quidem, commodi blanditiis eveniet vero aspernatur nobis atque molestias ratione
                         est voluptates officiis libero eligendi.</p>
                 </div>
             </div>
 
-            <div class="informacoesNutricionais centraliza">
-                <p>INFORMAÇÕES NUTRICIONAIS</p>
+            <div class="bg-white px-3 py-4 mt-4 d-flex justify-content-between align-items-center w-100 shadow">
+                <span
+                    style="color: #8C6342; font-family:'Poppins', sans-serif; font-weight:500; font-size: 0.8em ">INFORMAÇÕES
+                    NUTRICIONAIS</span>
                 <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
             </div>
         </div>
 
-        <div class="complemento">
-            <div class="adicionais centraliza">
-                <h2>Adicionais</h2>
-                <p>Escolha até 5 opções!</p>
+        <div class="my-5" style="font-family: 'Poppins', sans-serif;">
+            <div class="px-3 pb-4"
+                style="background-color: #d9d9d9; padding-top 2px; box-shadow: 0px 4px 8px rgba(0,0,0,0.1); ">
+                <span class="d-block fs-2 fw-semibold pt-2" style="color:#4a4a4a;">Adicionais</span>
+                <span class="d-block" style="font-weight:500;">Escolha até 5 opções!</span>
             </div>
 
-            <div class="itemAdicional centraliza">
-                <div class="descricaoAdicional">
-                    <h3>Adicionar Cebola</h3>
-                    <p>+ R$ 2,00</p>
+            <div class="bg-white px-3 mt-4 d-flex align-items-center justify-content-between"
+                style="height: 20vh; box-shadow: 0px 4px 8px rgba(0,0,0,0.1)">
+                <div>
+                    <h3 style="font-weight: 600; color: #464646">Adicionar Cebola</h3>
+                    <p class="fw-semibold">+ R$ 2,00</p>
                 </div>
 
                 <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
             </div>
 
-            <div class="itemAdicional centraliza">
-                <div class="descricaoAdicional">
-                    <h3>Adicionar Cebola</h3>
-                    <p>+ R$ 2,00</p>
-                </div>
-
-                <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
-            </div>
-        </div>
-
-        <div class="complemento">
-            <div class="adicionais centraliza">
-                <h2>Adicionais</h2>
-                <p>Escolha até 5 opções!</p>
-            </div>
-
-            <div class="itemAdicional centraliza">
-                <div class="descricaoAdicional">
-                    <h3>Adicionar Cebola</h3>
-                    <p>+ R$ 2,00</p>
-                </div>
-
-                <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
-            </div>
-
-            <div class="itemAdicional centraliza">
-                <div class="descricaoAdicional">
-                    <h3>Adicionar Cebola</h3>
-                    <p>+ R$ 2,00</p>
+            <div class="bg-white px-3 mt-4 d-flex align-items-center justify-content-between"
+                style="height: 20vh; box-shadow: 0px 4px 8px rgba(0,0,0,0.1)">
+                <div>
+                    <h3 style="font-weight: 600; color: #464646">Adicionar Cebola</h3>
+                    <p class="fw-semibold">+ R$ 2,00</p>
                 </div>
 
                 <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
             </div>
         </div>
 
-        <div id="botaoTelaProduto">
-            <button class="botaoAddCartTelaProduto">Add to Cart</button>
+        <div class="my-5" style="font-family: 'Poppins', sans-serif;">
+            <div class="px-3 pb-4"
+                style="background-color: #d9d9d9; padding-top 2px; box-shadow: 0px 4px 8px rgba(0,0,0,0.1); ">
+                <span class="d-block fs-2 fw-semibold pt-2" style="color:#4a4a4a;">Adicionais</span>
+                <span class="d-block" style="font-weight:500;">Escolha até 5 opções!</span>
+            </div>
+
+            <div class="bg-white px-3 mt-4 d-flex align-items-center justify-content-between"
+                style="height: 20vh; box-shadow: 0px 4px 8px rgba(0,0,0,0.1)">
+                <div>
+                    <h3 style="font-weight: 600; color: #464646">Adicionar Cebola</h3>
+                    <p class="fw-semibold">+ R$ 2,00</p>
+                </div>
+
+                <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
+            </div>
+
+            <div class="bg-white px-3 mt-4 d-flex align-items-center justify-content-between"
+                style="height: 20vh; box-shadow: 0px 4px 8px rgba(0,0,0,0.1)">
+                <div>
+                    <h3 style="font-weight: 600; color: #464646">Adicionar Cebola</h3>
+                    <p class="fw-semibold">+ R$ 2,00</p>
+                </div>
+
+                <i class="fa-solid fa-plus" style="color: #8C6342;"></i>
+            </div>
         </div>
 
+        <div class="posicionaBotaoSubmit">
+            <a href="{{ route('site.index') }}" class="botaoAdicionar"
+                id="botaoCancelar">Voltar</a>
 
+            <button type="submit" class="botaoAdicionar">Adicionar ao carrinho</button>
+        </div>
     </main>
 @endsection
