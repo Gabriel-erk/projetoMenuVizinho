75de70456cda4b28b0b66c07897a53899acd6672
Gabriel Erick <seu-email@exemplo.com>
2024-10-03 21:34:24 -0300

    criando model para tabela carrinho

--- database/migrations/2024_10_03_235801_create_lista_carrinho_table.php
+++ database/migrations/2024_10_03_235801_create_lista_carrinho_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('lista_carrinho', function (Blueprint $table) {
             $table->id();
+            // vou precisar das info do usuário logado
+            $table->unsignedBigInteger('user_id'); // Relaciona com o usuário
+            // vou precisar das info do cartao do cliente, q estarão na tabela cartao_cliente
+            $table->unsignedBigInteger('cartao_cliente_id')->nullable(); // Relaciona com o cartão do cliente (opcional)
             $table->timestamps();
+
+            // Chaves estrangeiras
+            $table->foreign('user_id')->references('id')->on('usuarios')->onDelete('cascade');
+            $table->foreign('cartao_cliente_id')->references('id')->on('cartao_cliente')->onDelete('set null'); // Se o cartão for deletado, o campo fica null
         });
     }
 
